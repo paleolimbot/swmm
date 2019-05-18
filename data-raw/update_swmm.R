87601660a7763cf554bb26278ba179a5fc668f66
Dewey Dunnington <dewey@fishandwhistle.net>
2019-05-18 11:41:44 -0300

    first commit

---
+++ data-raw/update_swmm.R
@@ -0,0 +1,19 @@
+
+curl::curl_download(
+  "https://www.epa.gov/sites/production/files/2018-08/swmm51013_engine_0.zip",
+  "data-raw/swmm.zip"
+)
+
+if(dir.exists("data-raw/swmm")) unlink("data-raw/swmm", recursive = TRUE)
+dir.create("data-raw/swmm")
+
+utils::unzip("data-raw/swmm.zip", exdir = "data-raw/swmm")
+
+if(dir.exists("src/swmm")) unlink("src/swmm", recursive = TRUE)
+dir.create("src/swmm")
+
+utils::unzip("data-raw/swmm/source5_1_013.zip", exdir = "src/swmm")
+
+# cleanup
+unlink("data-raw/swmm", recursive = TRUE)
+unlink("data-raw/swmm.zip")
